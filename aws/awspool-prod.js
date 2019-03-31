392c150f66fc1698928a1776d25b5e1ddf2fce61
toni dy <toni.dy2@gmail.com>
2019-03-31 09:47:50 +0700

    initial commit

---
+++ aws/awspool-prod.js
@@ -0,0 +1,5 @@
+﻿var poolData = {
+    UserPoolId: 'ap-northeast-1_l5O34Z2WY', // Your user pool id here
+    ClientId: '1rrc4ebo55tjidonnk81dhv7mm' // Your client id here
+};
+var poolId = 'ap-northeast-1_l5O34Z2WY';
